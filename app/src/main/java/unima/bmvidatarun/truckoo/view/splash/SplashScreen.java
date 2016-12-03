43d6e76d880afac78ddab2e298c52cddf2a56bd8
Marko <mjeftic@mail.uni-mannheim.de>
2016-12-03 13:37:28 +0100

    asdad

--- app/src/main/java/unima/bmvidatarun/truckoo/view/splash/SplashScreen.java
+++ app/src/main/java/unima/bmvidatarun/truckoo/view/splash/SplashScreen.java
@@ -1,10 +1,47 @@
 package unima.bmvidatarun.truckoo.view.splash;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import unima.bmvidatarun.R;
+import unima.bmvidatarun.truckoo.view.home.HomeActivity;
 
 /**
  * Created by Mukizen on 02.12.2016.
  */
 
 public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash);
+
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
 }
